acf898a696d40535266114e4d5336787b09ba01a
Kat mcg <cathaumann@gmail.com>
2020-08-12 22:47:28 +0200

    Added very basic layout

---
+++ src/containers/StackBuilder/StackBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+
+class StackBuilder extends Component {
+  render() {
+    return (
+      <Auxiliary>
+        <div>Pancake Stack</div>
+        <div>Build Controls</div>
+      </Auxiliary>
+    );
+  }
+}
+
+export default StackBuilder;
