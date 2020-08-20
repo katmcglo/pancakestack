3f8a09692fe303badc9167bffb14dd3ac14b02a9
Kat mcg <cathaumann@gmail.com>
2020-08-20 15:03:54 +0200

    added buttons to remove and add ingredients. needs logic for when total is below zero

--- src/components/PancakeStack/BuildControls/BuildControl/BuildControl.js
+++ src/components/PancakeStack/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import classes from './BuildControl.module.css';
 
 
 const buildControl = (props) => (
-  <div>
-    <div>{props.label}</div>
-    <button>Less</button>
-    <button>More</button>
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button onClick={props.removed} className={classes.Less}>Less</button>
+    <button onClick ={props.added} className={classes.More}>More</button>
   </div>
 );
 
--- src/components/PancakeStack/BuildControls/BuildControls.js
+++ src/components/PancakeStack/BuildControls/BuildControls.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import BuildControl from '../BuildControls/BuildControl/BuildControl';
+import classes from '../BuildControls/BuildControls.module.css';
 
+const controls = [
+  { label: 'Salad', type: 'salad'},
+  { label: 'Bacon', type: 'bacon'},
+  { label: 'Cheese', type: 'cheese'},
+  { label: 'Meat', type: 'meat'}
+];
 
 const buildControls = (props) => (
-  <div>
-    
+  <div className={classes.BuildControls}>
+    {controls.map(control => (
+    <BuildControl 
+    key={control.label} 
+    label={control.label}
+    added={() => props.ingredientAdded(control.type)}
+    removed={() => props.ingredientRemoved(control.type)}
+      />
+    ))}
   </div>
 );
 
